4468383595d6a5ff99addd63da6b1cbe5bd1fff3
patrykbrzozowski <patrykbrzozula@gmail.com>
2022-03-16 07:34:22 +0100

    Add BMI_calculator app

--- fit_app/calculators/views.py
+++ fit_app/calculators/views.py
@@ -1,7 +1,41 @@
+from os import stat
 from django.shortcuts import render
+from .models import BMI
 
 # Create your views here.
 def BMI_calculator(request):
     context = {}
+    if request.method == 'POST':
+        weight = float(request.POST.get('weight'))
+        height = float(request.POST.get('height')) / 100
 
-    return render(request, 'calculators/BMI_calculator.html')
+        bmi = weight / height ** 2
+        save = request.POST.get('save')
+        if save == 'on':
+            BMI.objects.create(user=request.user, weight=weight, height=height, bmi=round(bmi,1))
+
+        #print(int(bmi), "+", bmi)
+
+        if bmi < 16:
+            state = "Wygłodzenie"
+        elif 16 < bmi < 16.99:
+            state = "Wychudzenie"
+        elif 17 < bmi < 18.49:
+            state = "Niedowaga"
+        elif 18.5 < bmi < 24.99:
+            state = "Waga prawidłowa"
+        elif 25 < bmi < 29.99:
+            state = "Nadwaga"
+        elif 30 < bmi < 34.99:
+            state = "Otyłość I Stopnia"
+        elif 35 < bmi < 39.99:
+            state = "Otyłość II stopnia (tzw. Kliniczna)"
+        elif bmi > 40:
+            state = "Otyłość III stopnia"
+
+        #print(state, "+", bmi)
+
+        context['bmi'] = round(bmi, 1)
+        context['state'] = state
+
+    return render(request, 'calculators/BMI_calculator.html', context)
