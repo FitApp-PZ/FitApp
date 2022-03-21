e0b644538d7a58f70720806de8a8fcc7d8e9139e
patrykbrzozowski <patrykbrzozula@gmail.com>
2022-03-21 16:07:16 +0100

    Add hydration app

--- fit_app/fit_app/settings.py
+++ fit_app/fit_app/settings.py
@@ -25,8 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'calculators',
     'website',
+    'hydration',
 ]
 
 MIDDLEWARE = [
