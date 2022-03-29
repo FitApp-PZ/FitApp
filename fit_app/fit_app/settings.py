ffe3c90ecfa0938cf238504ef2d9f54b5d37f232
patrykbrzozowski <patrykbrzozula@gmail.com>
2022-03-29 13:54:33 +0200

    Add accounts App and fix views

--- fit_app/fit_app/settings.py
+++ fit_app/fit_app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'activities',
+    'accounts',
     'calculators',
     'sleep',
     'website',
--- fit_app/fit_app/urls.py
+++ fit_app/fit_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('accounts/', include('accounts.urls')),
     path('calculators/', include('calculators.urls')),
     path('hydration/', include('hydration.urls')),
     path('activities/', include('activities.urls')),
