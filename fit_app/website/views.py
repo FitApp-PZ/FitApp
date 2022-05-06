366467e68fe4f04a2230991963f14d0fb895bb96
patrykbrzozowski <patrykbrzozula@gmail.com>
2022-05-06 08:50:47 +0200

    Change view of dashboard

--- fit_app/website/views.py
+++ fit_app/website/views.py
@@ -1,10 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.db.models import Sum, Q
-from datetime import date
+from datetime import date, datetime, timedelta
 
 from activities.models import Activity
 from hydration.models import Water
 from sleep.models import Sleep
+from calculators.models import BMI
 from body_measurments.models import BodyMeasurements
 
 # Create your views here.
@@ -44,7 +46,17 @@ def dashboard(request):
     else:
         current_weight = current_weight['weight']
     
+    current_height = BodyMeasurements.objects.filter(user = request.user).values('height').order_by('-date').first()
+    if current_height is None:
+        current_height = 'Nie dodano żadnego wzrostu'
+    else:
+        current_height = current_height['height']
 
+    current_BMI = BMI.objects.filter(user = request.user).values('bmi').order_by('-date').first()
+    if current_BMI is None:
+        current_BMI = 'Nie dodano żadnego BMI'
+    else:
+        current_BMI = current_BMI['bmi']
 
     context = {
         'sum_of_water_today': sum_of_water_today/1000,
@@ -53,9 +65,12 @@ def dashboard(request):
         'width_of_sleep_bar': width_of_sleep_bar,
         'width_of_activities_bar': width_of_activities_bar,
         'width_of_water_bar': width_of_water_bar,
-        'current_weight': current_weight
+        'current_weight': current_weight,
+        'current_height': current_height,
+        'current_BMI': current_BMI
     }
     return render(request, 'website/dashboard.html', context=context)
+
 
 def BMI_calculator(request):
     return render(request, 'website/BMI_calculator.html')
