ca777598daae384bb6b0d3cf569a8113bb838770
Marek Baczewski <marekbacz0808@gmail.com>
2022-05-17 17:03:54 +0200

    reset hasla

--- fit_app/accounts/urls.py
+++ fit_app/accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     # path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
-            path('password_reset/', views.password_reset_request, name='password_reset'),
+    path('password_reset/', views.password_reset_request, name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
     path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm')
